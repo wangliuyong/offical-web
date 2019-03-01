f015cd2ec978065be30bb534f5dc96b231ce9211
wangliuyong <1355498705@qq.com>
2019-03-01 08:58:46 +0800

    管理端页框架搭建完毕

---
+++ miniprogram/untils/data.js
@@ -0,0 +1,27 @@
+function onAdd(collection, data = {}) {
+  const db = wx.cloud.database()
+  db.collection(collection).add({
+    data,
+    success: res => {
+      // 在返回结果中会包含新创建的记录的 _id
+      this.setData({
+        counterId: res._id,
+        count: 1
+      })
+      wx.showToast({
+        title: '新增记录成功',
+      })
+      console.log('[数据库] [新增记录] 成功，记录 _id: ', res._id)
+    },
+    fail: err => {
+      wx.showToast({
+        icon: 'none',
+        title: '新增记录失败'
+      })
+      console.error('[数据库] [新增记录] 失败：', err)
+    }
+  })
+}
+
+
+export default onAdd;
